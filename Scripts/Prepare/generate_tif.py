b5674614463c2206f1d0246696c52bb2d872dc64
Patrick Broxton <broxtopd@users.noreply.github.com>
2018-08-08 19:59:00 -0700

    Update generate_tif.py

--- Scripts/Prepare/generate_tif.py
+++ Scripts/Prepare/generate_tif.py
@@ -20,7 +20,8 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
-###############################################################################import sys,os
+###############################################################################
+import sys,os
 import zipfile
 import gdal, osr
 import subprocess
